00c9d59845908e7bbabe2195c3f574637d7f0f6a
Suhail Hasan Khan <shasankhan@usf.edu>
2025-10-17 14:17:03 -0400

    added the character counter for the comments section.

--- cp_1.js
+++ cp_1.js
@@ -5,3 +5,11 @@ const form = document.getElementById("feedback-form");
 const submittedFeedback = document.getElementById("listFeedback");
 const tooltip = document.getElementById("tooltip");
 const counter = document.getElementById("char-count");
+
+
+form.addEventListener("input", (e) => {
+  let t = e.target;
+  if (t.id === "comments") {
+    counter.textContent = `${t.value.length} characters`;
+  }
+});
