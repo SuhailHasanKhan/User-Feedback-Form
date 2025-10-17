02cc3f563f7a8666944be092f0b0faf900e60a9a
Suhail Hasan Khan <shasankhan@usf.edu>
2025-10-17 14:05:06 -0400

    Used getElementById to grab the required items

--- cp_1.js
+++ cp_1.js
@@ -1,2 +1,7 @@
 // Coding Project 1
 
+
+const form = document.getElementById("feedback-form");
+const submittedFeedback = document.getElementById("listFeedback");
+const tooltip = document.getElementById("tooltip");
+const counter = document.getElementById("char-count");
