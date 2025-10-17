8b8adb165177736c6b6f83c2a30832697910824c
Suhail Hasan Khan <shasankhan@usf.edu>
2025-10-17 14:45:56 -0400

    added tooltip function and also styled it.

--- cp_1.js
+++ cp_1.js
@@ -14,3 +14,23 @@ form.addEventListener("input", (e) => {
   }
 });
 
+form.addEventListener("mouseover", (e) => {
+  let t = e.target;
+  if (!t.matches("input, textarea, button, label")) return;
+  let tTip = t.getAttribute("data-tooltip");
+  if (!tTip) return;
+  tooltip.textContent = tTip;
+  tooltip.hidden = false;
+});
+
+form.addEventListener("mouseout", (e) => {
+  let t = e.target;
+  if (!t.matches("input, textarea, button, label")) return;
+  tooltip.hidden = true;
+});
+
+form.addEventListener("mousemove", (e) => {
+  if (tooltip.hidden) return;
+  tooltip.style.left = (e.pageX + 5) + "px";
+  tooltip.style.top = (e.pageY + 5) + "px";
+});
